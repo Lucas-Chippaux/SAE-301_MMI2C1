d90b78a75788d3665b4164d5ce0222cfaf0ab89e
Lucas-Chippaux <116551442+Lucas-Chippaux@users.noreply.github.com>
2023-10-26 12:27:04 +0200

    Update personnalisation.vue

--- src/views/personnalisation.vue
+++ src/views/personnalisation.vue
@@ -1,39 +1,46 @@
 <template>
-    <div>
-      <div class="perso-container mt-10">
-        <div class="perso-title flex justify-start">
-          <img class="w-180 h-80 mt-40 ml-20" src="@/assets/img/lunette_perso.svg" alt="Image de la paire de lunettes">
-          <h2 class="text-2xl font-palanquin font-bold text-bleufonce ml-60">Personnaliser votre paire de lunette</h2>
-        </div>
+       <div class="perso-container mt-10">
+    <div class="perso-title">
+    </div>
+    <h2 class="text-3xl font-palanquin font-bold text-bleufonce mx-auto text-center">Personnaliser votre paire de lunette</h2>
       </div>
-  
-      <div class="perso-container mt-10">
-        <div class="perso-title flex justify-start">
-          <h2 class="text-2xl font-palanquin font-bold text-bleufonce ml-10">Description</h2>
-        </div>
-        <div>
-          <p class="font-nunito text-sm ml-10 mt-5">
-            "Lyon Lumière" est une paire de lunettes qui capture la beauté de la lumière lyonnaise. Ces lunettes unisexes allient un design minimaliste à une touche de sophistication.
-            Fabriquées en France, elles incarnent l'artisanat de qualité et offrent un confort visuel inégalé.
-          </p>
-        </div>
+
+    <!-- personnalistion -->
+    <ConfigurateurView />
+    <div class="flex">
+
+    <div class="perso-container mt-10">
+      <div class="perso-title flex justify-start">
+        <h2 class="text-2xl font-palanquin font-bold text-bleufonce ml-10">Description</h2>
       </div>
-  
-      <div class="perso-container mt-10">
-        <div class="perso-title flex justify-start">
-          <h2 class="text-2xl font-palanquin font-bold text-bleufonce ml-10">Caractéristiques</h2>
-        </div>
-        <div>
-          <p class="font-nunito text-sm ml-10 mt-5">
-            Chez TaVue, nous mettons à votre disposition un large éventail de caractéristiques de personnalisation pour créer la paire de lunettes de vos rêves.
-            Chaque détail est soigneusement pensé pour vous permettre de personnaliser votre style, votre confort et votre expérience visuelle. Possibilité d’appliquer une protection optimale contre les rayons UV.
-          </p>
-          <p class="font-nunito text-sm ml-10 mt-5 mb-20">
-            Explorez les possibilités infinies de personnalisation pour créer une paire de lunettes qui est vraiment la vôtre. Chez TaVue, nous mettons l'accent sur la qualité, le style et l'individualité.
-            Découvrez l'expérience unique de la personnalisation des lunettes et créez la paire parfaite pour vous.
-          </p>
-        </div>
+      <div>
+        <p class="font-nunito text-sm ml-10 mt-5">
+          "Lyon Lumière" est une paire de lunettes qui capture la beauté de la lumière lyonnaise. Ces lunettes unisexes allient un design minimaliste à une touche de sophistication.
+          Fabriquées en France, elles incarnent l'artisanat de qualité et offrent un confort visuel inégalé.
+        </p>
       </div>
     </div>
-  </template>
+
+    <div class="perso-container mt-10">
+      <div class="perso-title flex justify-start">
+        <h2 class="text-2xl font-palanquin font-bold text-bleufonce ml-10">Caractéristiques</h2>
+      </div>
+      <div>
+        <p class="font-nunito text-sm ml-10 mt-5">
+          Chez TaVue, nous mettons à votre disposition un large éventail de caractéristiques de personnalisation pour créer la paire de lunettes de vos rêves.
+          Chaque détail est soigneusement pensé pour vous permettre de personnaliser votre style, votre confort et votre expérience visuelle. Possibilité d’appliquer une protection optimale contre les rayons UV.
+        </p>
+        <p class="font-nunito text-sm ml-10 mt-5 mb-20">
+          Explorez les possibilités infinies de personnalisation pour créer une paire de lunettes qui est vraiment la vôtre. Chez TaVue, nous mettons l'accent sur la qualité, le style et l'individualité.
+          Découvrez l'expérience unique de la personnalisation des lunettes et créez la paire parfaite pour vous.
+        </p>
+      </div>
+    </div>
+    </div>
+</template>
+
+
+<script setup>
+import ConfigurateurView from '../components/ConfigurateurView.vue';
+</script>
   
